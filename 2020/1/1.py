f96a64fdd3a0f1c6f55fcd0d56d4da939ec185ec
Dave Zolotusky <dzolo@spotify.com>
2020-12-20 22:40:23 +0100

    add 2020.1

---
+++ 2020/1/1.py
@@ -0,0 +1,21 @@
+with open("input1.txt") as f:
+    content = f.readlines()
+
+numbers = [int(i) for i in content]
+
+part1 = 0
+part2 = 0
+
+for num1 in numbers:
+    for num2 in numbers:
+        if num1 + num2 == 2020:
+            part1 = num1 * num2
+            break
+        for num3 in numbers:
+            if num1 + num2 + num3 == 2020:
+                part2 = num1 * num2 * num3
+                break
+
+print("part 1 = " + str(part1))
+
+print("part 2 = " + str(part2))
