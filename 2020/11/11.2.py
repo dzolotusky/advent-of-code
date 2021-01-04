9ab132ce60732e01bffb0450b8dbda69a45fc0f6
Dave Zolotusky <dzolo@spotify.com>
2021-01-04 15:08:00 +0100

    2020.11

---
+++ 2020/11/11.2.py
@@ -0,0 +1,82 @@
+with open("input11.txt") as f:
+    content = f.readlines()
+
+part2 = 0
+
+grid = []
+for i in range(len(content)):
+    sub_array = ['.'] * (len(content[0]) - 1)
+    grid.append(sub_array)
+
+for i in range(len(grid)):
+    for j in range(len(grid[i])):
+        grid[i][j] = content[i][j]
+
+
+def print_grid():
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            print(grid[i][j], end='')
+        print()
+
+
+new_grid = []
+for i in range(len(content)):
+    sub_array = ['.'] * (len(content[0]) - 1)
+    new_grid.append(sub_array)
+
+
+def count_adjacent(test_grid, i, j):
+    num_occ = 0
+    for i_delta in range(-1, 2):
+        if -1 < i + i_delta < len(test_grid):
+            for j_delta in range(-1, 2):
+                if i_delta == j_delta == 0:
+                    continue
+                if -1 < j + j_delta < len(test_grid[i]):
+                    if test_grid[i+i_delta][j+j_delta] == "#":
+                        num_occ += 1
+                    else:
+                        if test_grid[i + i_delta][j + j_delta] == ".":
+                            steps = 2
+                            while(-1 < i + (steps * i_delta) < len(test_grid) and
+                                  -1 < j + (steps * j_delta) < len(test_grid[i])):
+                                if test_grid[i + steps * i_delta][j + steps * j_delta] == "#":
+                                    num_occ += 1
+                                if test_grid[i + steps * i_delta][j + steps * j_delta] != ".":
+                                    break
+                                steps += 1
+
+    return num_occ
+
+
+made_change = True
+while made_change:
+    new_grid = []
+    for i in range(len(content)):
+        sub_array = ['.'] * (len(content[0]) - 1)
+        new_grid.append(sub_array)
+
+    made_change = False
+
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            if grid[i][j] == "L" and count_adjacent(grid, i, j) == 0:
+                new_grid[i][j] = "#"
+                made_change = True
+            else:
+                if grid[i][j] == "#" and count_adjacent(grid, i, j) > 4:
+                    new_grid[i][j] = "L"
+                    made_change = True
+                else:
+                    new_grid[i][j] = grid[i][j]
+    grid = new_grid
+
+
+
+for i in range(len(grid)):
+    for j in range(len(grid[i])):
+        if grid[i][j] == "#":
+            part2 += 1
+
+print("part 2 = " + str(part2))
